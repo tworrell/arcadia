eee8ac5b53f8d39bde43752bc3b33a4ef6860776
Cliff <cliffhazelton@gmail.com>
2019-03-04 19:11:35 -0500

    Adds default `hit_points` and a MiniTest file for testing the character class

--- character.rb
+++ character.rb
@@ -1,9 +1,16 @@
 class Character
+
   attr_accessor :name,
                 :strength,
                 :dexterity,
                 :intelligence,
                 :wisdom,
                 :constitution,
-                :charisma
+                :charisma,
+                :hit_points
+
+  def initialize
+    @hit_points = 100
+  end
+
 end
---
+++ character_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative 'character'
+
+class TestCharacter < MiniTest::Unit::TestCase
+
+  def test_example_test
+    assert_equal(2, 1 + 1)
+  end
+
+end
