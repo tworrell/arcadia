9c2fc29e19f73f929f1ff621308f44bca891d499
Cliff <cliffhazelton@gmail.com>
2019-03-11 18:17:36 -0400

    Added a test for default hit_points

--- character_test.rb
+++ character_test.rb
@@ -1,10 +1,12 @@
 require 'minitest/autorun'
+
 require_relative 'character'
 
 class TestCharacter < MiniTest::Unit::TestCase
 
-  def test_example_test
-    assert_equal(2, 1 + 1)
+  def test_character_hit_points_defaults_to_100
+    character = Character.new
+    assert_equal(100, character.hit_points)
   end
 
 end
